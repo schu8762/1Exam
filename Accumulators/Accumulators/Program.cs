eabfeeafb20a113f15dd47282c0a60603362a224
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-09-29 18:14:21 -0500

    accumulator

---
+++ Accumulators/Accumulators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Accumulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int counter;
+            int sum = 0;
+            double mult = 1;
+
+            for (counter = 1; counter < 101; counter++)
+            {
+                sum += counter;
+                mult *= counter;
+
+            }
+            Console.WriteLine($" {sum.ToString("n0")} and { mult.ToString("N0")}");
+
+            Console.ReadKey();
+        }
+    }
+}
