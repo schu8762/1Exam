8233ab6d66cec139b4e7e3c54de8dc2e4d543095
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 18:55:29 -0500

    next

---
+++ Loops(1)redoHW/Loops(1)redoHW/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Loops_1_redoHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double minuteCals = 3.9;
+            int minutes = 1;
+
+            Console.WriteLine("How many calories do you wish to burn?");
+            double calories = Convert.ToDouble(Console.ReadLine());
+
+            double burnedCals = 0;
+
+            while (calories % burnedCals != 0 && calories != burnedCals && calories > burnedCals)
+            {
+                burnedCals = minutes * minuteCals;
+                Console.WriteLine($"In {minutes} minute(s) you have burned {burnedCals}");
+                minutes++;
+
+                if (calories == burnedCals)
+                {
+                    Console.WriteLine($"it took you {minutes} minutes");
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sum/sum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            for (int counter = 1; counter < 101; counter++)
+            {
+                sum =+ counter;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
