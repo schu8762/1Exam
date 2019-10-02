4eebc2e3edcd46083f5c45f9dc501e5817963b88
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 21:12:51 -0500

    coin toss

---
+++ CoinToss/CoinToss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int toss = rand.Next(1, 3);
+            string toss2;
+            string answer;
+
+            if (toss == 1)
+            {
+                toss2 = "tails";
+            }
+            else
+            {
+                toss2 = "heads";
+            }
+
+            Console.WriteLine($"{toss}");
+
+            do
+            {
+                Console.WriteLine("Heads or tails???");
+                answer = Console.ReadLine().ToLower();
+                
+            } while (answer != toss2);
+
+            Console.WriteLine("congrats, you got it!");
+
+            Console.ReadKey();
+        }
+    }
+}
