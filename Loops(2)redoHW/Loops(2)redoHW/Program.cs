33dcdd8f3dbe9498b7dcef1cdbc96a7039be8cc2
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 19:08:18 -0500

    loops2

---
+++ Loops(2)redoHW/Loops(2)redoHW/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops_2_redoHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Random rand2 = new Random();
+
+            int generatedNumber = rand.Next(1, 51);
+            int generatedNumber2 = rand.Next(1, 51);
+
+            int sum = generatedNumber + generatedNumber2;
+            int ans = 0;
+                       
+            do
+            {
+                Console.WriteLine($"what is the sum of {generatedNumber} + {generatedNumber2} ?");
+                ans = Convert.ToInt32(Console.ReadLine());
+            } while (ans != sum);
+        }
+    }
+}
