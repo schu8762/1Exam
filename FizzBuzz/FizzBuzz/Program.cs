a403e7ca11a44ce8c167332fe5008efaba8d2641
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 20:52:17 -0500

    fizz buzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string sentence = "sorry youre stupid";
+
+            Console.WriteLine("Enter your favorite number!");
+            string favNumber = Console.ReadLine();
+
+            while (Int32.TryParse(favNumber, out number) == false)
+            {
+                Console.WriteLine("Invalid input, please enter your favorite number!");
+                favNumber = Console.ReadLine();
+            }
+              
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine($"{number} Fizz Buzz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine($"{number} Fizz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine($"{number} Buzz");
+            }          
+            else
+            {
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(sentence[i]);
+                }
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
