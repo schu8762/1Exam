f9f694ad995d62537e794fa3d67e942fe4ddc079
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 19:34:32 -0500

    hw1

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double salesTax = .085;
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            string item = Console.ReadLine();
+
+            Console.WriteLine($"How many {item}s would you like to buy");
+            double quantity = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"What is the price for each {item} ?");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            double subtotal = price * quantity;
+            double salesTaxTotal = subtotal * salesTax;
+            double totalBill = subtotal + salesTaxTotal;
+
+            Console.WriteLine($"Your subtotal for your bill is {subtotal.ToString("c2")}. \n Your sales tax for your bill is {salesTaxTotal.ToString("c2")}. \n Your total for your bill is {totalBill.ToString("c2")}.");
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
