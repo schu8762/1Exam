597e616d48b79e573403a2d82902c4b9c4886519
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 19:17:27 -0500

    tuition

---
+++ Loops(3)redoHW/Loops(3)redoHW/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loops_3_redoHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tuition = 12600;
+            int years = 0;
+            const double incr = 1.05;
+
+            for (years = 0; years < 8; years++)
+            {
+                tuition *= incr;
+                Console.WriteLine($"Tuition in year {years} is {tuition.ToString("c2")}");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
