89bc04b17489027ebb6fea26ebb9e5ff4ff40f38
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 22:07:58 -0500

    daysofmonth2

---
+++ DaysofMonth2/DaysofMonth2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DaysofMonth2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 0;
+            Console.WriteLine("What day of the month is it?");
+            string month = Console.ReadLine().ToLower();
+
+            if (month == "january" || month == "march" || month == "may" || month == "july" || month == "august" || month == "october" || month == "december")
+            {
+                days = 31;
+            }
+            else if (month == "february")
+            {
+                Console.WriteLine("Is it a leap year? yes/no");
+                string leap = Console.ReadLine().ToLower();
+                
+               if (leap == "yes")
+                {
+                    days = 29;
+                }
+               else if (leap == "no")
+                {
+                     days = 28;
+                }
+
+            }
+            else 
+            {
+                days = 30;
+            }
+
+            Console.WriteLine($"There are {days} days in {month}");
+            Console.ReadKey();
+        }
+    }
+}
