95da19cffb68c8fc49e23345595ba3edb7c10520
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-01 22:32:53 -0500

    acc

---
+++ Accumulator2/Accumulator2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Accumulator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            double mult = 1;
+
+            for (int counter = 1; counter < 101; counter++)
+            {
+               sum += counter;
+              // mult *= counter; 
+               //Console.WriteLine(sum);
+            }
+            for (int counter = 1; counter < 101; counter++)
+            {
+                //sum += counter;
+                mult *= counter; 
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(mult.ToString("n2"));
+            Console.ReadKey();
+        }
+    }
+}
