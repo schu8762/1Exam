237dc519967c5451b8d1fc3c0467e1417a57998e
Jacob.Schuller <Jacob.Schuller@WAMJLLG8326DTB>
2019-10-02 22:20:02 -0500

    last commit

---
+++ screwaround2/screwaround2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace screwaround2
+{
+    class Program
+    {
+        static string sentence = "This class is pretty dang awesome";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sentence);
+            Console.WriteLine("what is your favorite word in the above sentence?");
+            string word = Console.ReadLine();
+
+            if (sentence.Contains(word))
+            {
+                Console.WriteLine("what do you want to change it to?");
+                string newWord = Console.ReadLine();
+                string newSentence = sentence.Replace(word, newWord);
+                Console.WriteLine(newSentence);
+            }
+            else
+            
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(word[i]);
+                }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
